dd72f5c7c2a5d540ba42f80219d2dc502049fc3a
Túlio Farias Fracaro <tuliofariasfracaro@hotmail.com>
2023-11-16 09:25:25 -0300

    ok

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,13 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import lojaController from './src/controller/lojaController.js';
+//import lojaController from './src/controller/lojaController.js';
 
 const servidor = express();
 servidor.use(cors());
 servidor.use(express.json());
 
-servidor.use(lojaController);
+//servidor.use(lojaController);
 
 const port = process.env.PORT;
-servidor.listen(port, () => console.log(`API subiu na porta ${port}!`));
+servidor.listen(port, () => console.log(`API subiu na porta ${port}  !`));
